7355bf58979b815c2cb438ea41bfc39cfb590943
jtx007 <jamesjacobthomas7@gmail.com>
2018-08-22 16:58:36 -0400

    lab code

--- src/components/CocktailsList.js
+++ src/components/CocktailsList.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import Cocktail from './Cocktail'
 
 class CocktailsList extends Component {
+
+  genCocktailComponent(cocktails) {
+  return cocktails.map((cocktail) =>  {return <Cocktail key={cocktail.name} cocktail={cocktail} displayCocktail={this.props.displayCocktail} />})
+  }
+
   render(){
     return (
       <div id="cocktail-list">
-
+        {this.genCocktailComponent(this.props.cocktails)}
       </div>
     )
   }
